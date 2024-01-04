f51379aeba0207a2fdb8beb7b1a63727b38ae167
Hendrik Langer <hendrik+dev@xd0.de>
2024-01-04 21:19:36 +0100

    update

--- create.sh
+++ create.sh
@@ -1,20 +1,20 @@
 echo "preclean"
 rm -r ./src/
-rm -fr tflite-micro-esp-examples
+rm -fr esp-tflite-micro
 rm -fr esp-nn
 
 echo "cloning espressif tensorflow examples"
-git clone --recurse-submodules https://github.com/espressif/tflite-micro-esp-examples.git
+git clone --recurse-submodules https://github.com/espressif/esp-tflite-micro.git
 
 echo "Making tensorflow files structured for PIO/Arduino"
 mkdir ./src/
-cp -r ./tflite-micro-esp-examples/components/tflite-lib/tensorflow/ ./src/tensorflow/
-cp -r ./tflite-micro-esp-examples/components/tflite-lib/third_party/flatbuffers/include/flatbuffers/ ./src/
+cp -r ./esp-tflite-micro/tensorflow/ ./src/tensorflow/
+cp -r ./esp-tflite-micro/third_party/flatbuffers/include/flatbuffers/ ./src/
 sed -i 's/utility.h/utility/g' ./src/flatbuffers/base.h
-cp -r ./tflite-micro-esp-examples/components/tflite-lib/third_party/gemmlowp/fixedpoint/ ./src/  
-cp -r ./tflite-micro-esp-examples/components/tflite-lib/third_party/gemmlowp/internal/ ./src/
-cp -r ./tflite-micro-esp-examples/components/tflite-lib/third_party/kissfft/ ./src/  
-cp -r ./tflite-micro-esp-examples/components/tflite-lib/third_party/ruy/ruy/ ./src/
+cp -r ./esp-tflite-micro/third_party/gemmlowp/fixedpoint/ ./src/  
+cp -r ./esp-tflite-micro/third_party/gemmlowp/internal/ ./src/
+cp -r ./esp-tflite-micro/third_party/kissfft/ ./src/  
+cp -r ./esp-tflite-micro/third_party/ruy/ruy/ ./src/
 cp -a ./src/kissfft/tools/. ./src/kissfft/
 rm -r ./src/kissfft/tools/
 find ./src/tensorflow/ -type f -exec sed -i -e 's/#include "kiss_fft.c"/#include "kissfft\/kiss_fft.c"/g' {} \;
@@ -29,16 +29,17 @@ cp -a ./src/tensorflow/lite/micro/kernels/esp_nn/. ./src/tensorflow/lite/micro/k
 sed -i 's/#define TENSORFLOW_LITE_C_COMMON_H_/#define TENSORFLOW_LITE_C_COMMON_H_\n#define ESP_NN 1/g' ./src/tensorflow/lite/c/common.h
 
 echo "Making esp-nn files structured for PIO/Arduino"
+git clone --recurse-submodules https://github.com/espressif/esp-nn.git
 mkdir ./src/esp-nn
 
-cp -a  ./tflite-micro-esp-examples/components/esp-nn/src/activation_functions/. ./src/esp-nn/
-cp -a  ./tflite-micro-esp-examples/components/esp-nn/src/basic_math/. ./src/esp-nn/
-cp -a  ./tflite-micro-esp-examples/components/esp-nn/src/common/. ./src/esp-nn/
-cp -a  ./tflite-micro-esp-examples/components/esp-nn/src/convolution/. ./src/esp-nn/
-cp -a  ./tflite-micro-esp-examples/components/esp-nn/src/fully_connected/. ./src/esp-nn/
-cp -a  ./tflite-micro-esp-examples/components/esp-nn/src/pooling/. ./src/esp-nn/
-cp -a  ./tflite-micro-esp-examples/components/esp-nn/src/softmax/. ./src/esp-nn/
-cp -a  ./tflite-micro-esp-examples/components/esp-nn/include/. ./src/esp-nn/
+cp -a  ./esp-nn/src/activation_functions/. ./src/esp-nn/
+cp -a  ./esp-nn/src/basic_math/. ./src/esp-nn/
+cp -a  ./esp-nn/src/common/. ./src/esp-nn/
+cp -a  ./esp-nn/src/convolution/. ./src/esp-nn/
+cp -a  ./esp-nn/src/fully_connected/. ./src/esp-nn/
+cp -a  ./esp-nn/src/pooling/. ./src/esp-nn/
+cp -a  ./esp-nn/src/softmax/. ./src/esp-nn/
+cp -a  ./esp-nn/include/. ./src/esp-nn/
 # make esp_nn includes work
 find ./src/esp-nn/ -type f -exec sed -i -e 's/#include <common_functions.h>/#include "common_functions.h"/g' {} \;
 find ./src/esp-nn/ -type f -exec sed -i -e 's/#include <esp_nn_defs.h>/#include "esp_nn_defs.h"/g' {} \;
@@ -47,7 +48,7 @@ find ./src/tensorflow/ -type f -exec sed -i -e 's/#include <esp_nn.h>/#include "
 
 echo "Clean up"
 #clean up 
-rm -fr tflite-micro-esp-examples
+rm -fr esp-tflite-micro
 rm -fr esp-nn
 
 echo "Libary update done."
